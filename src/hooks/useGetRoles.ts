4296728f783422caa991ae1879a104e70fc741ec
taica00 <64315786+taica00@users.noreply.github.com>
2025-01-27 01:03:22 +0800

    comment

--- src/hooks/useGetRoles.ts
+++ src/hooks/useGetRoles.ts
@@ -2,6 +2,7 @@ import { mockRolesList } from "@/mocks/mockRoles"
 import { useQuery } from "@tanstack/react-query"
 
 const useGetRoles = () => {
+    // to be replaced with real api call
     return useQuery({queryKey: ['roles'], queryFn: () => mockRolesList})
 }
 
