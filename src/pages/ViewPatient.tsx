383ef4bdb75c1d6ad359ff418d198577e72b5dc0
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-02-06 16:44:58 +0800

    remove using mock data for view patient

--- src/pages/ViewPatient.tsx
+++ src/pages/ViewPatient.tsx
@@ -50,7 +50,7 @@ const ViewPatient: React.FC = () => {
       const fetchedData: ProfilePhotoAndName =
         import.meta.env.MODE === 'development' ||
         import.meta.env.MODE === 'production'
-          ? mockProfilePhotoAndName//await fetchProfilePhotoAndName(Number(id))
+          ? await fetchProfilePhotoAndName(Number(id))
           : mockProfilePhotoAndName;
       // console.log(fetchedData);
       setProfilePhotoAndName(fetchedData);
