96041a86744e86d357e6a6bb731ded5033d3232a
taica00 <64315786+taica00@users.noreply.github.com>
2025-02-17 23:43:30 +0800

    users as prefix

--- src/hooks/role/useGetUsersFromRole.ts
+++ src/hooks/role/useGetUsersFromRole.ts
@@ -2,7 +2,7 @@ import { getUsersFromRole } from "@/api/role/roles"
 import { useQuery } from "@tanstack/react-query"
 
 const useGetUsersFromRole = (roleName: string) => {
-  return useQuery({ queryKey: [roleName, 'users'], queryFn: () => getUsersFromRole(roleName) })
+  return useQuery({ queryKey: ['users', roleName], queryFn: () => getUsersFromRole(roleName) })
 }
 
 export default useGetUsersFromRole
