bd477e01b1e6cbe39eb262012fbdebd9c77e8ac6
Lee Chun Yang <40287858+leechunyang98@users.noreply.github.com>
2025-03-14 05:37:22 +0800

    refactor delete modal ui into BaseDeleteModal

--- src/components/Modal/DeletePatientModal.tsx
+++ src/components/Modal/DeletePatientModal.tsx
@@ -1,7 +1,7 @@
-import { Button } from "../ui/button";
 import { toast } from "sonner";
 import { useModal } from "@/hooks/useModal";
 import { deletePatient } from "@/api/patients/patients";
+import BaseDeleteModal from "./BaseDeleteModal";
 
 const DeletePatientModal: React.FC = () => {
   const { modalRef, activeModal, closeModal } = useModal();
@@ -10,7 +10,7 @@ const DeletePatientModal: React.FC = () => {
     refreshData: () => void;
   };
 
-  const handleMobilityAid = async (event: React.FormEvent) => {
+  const handleDeletePatient = async (event: React.FormEvent) => {
     event.preventDefault();
     if (!patientId || isNaN(Number(patientId))) return;
     try {
@@ -26,29 +26,13 @@ const DeletePatientModal: React.FC = () => {
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div ref={modalRef} className="bg-background rounded-md w-[500px]">
-        <div className="bg-destructive p-4">
-          <h3 className="text-lg font-medium text-destructive-foreground">
-            Are you sure?
-          </h3>
-        </div>
-
-        <form onSubmit={handleMobilityAid} className="p-4">
-          <p className="mb-6 text-gray-600 dark:text-gray-200 text-center">
-            Deleting this item is irreversible. Please confirm your action.
-          </p>
-          <div className="col-span-2 mt-6 flex justify-end space-x-2">
-            <Button variant="outline" onClick={closeModal}>
-              Cancel
-            </Button>
-            <Button type="submit" variant="destructive">
-              Delete
-            </Button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <>
+      <BaseDeleteModal
+        modalRef={modalRef}
+        onSubmit={handleDeletePatient}
+        closeModal={closeModal}
+      />
+    </>
   );
 };
 
--- src/components/Modal/DeleteVitalModal.tsx
+++ src/components/Modal/DeleteVitalModal.tsx
@@ -1,7 +1,7 @@
 import { deleteVital } from "@/api/patients/vital";
-import { Button } from "../ui/button";
 import { toast } from "sonner";
 import { useModal } from "@/hooks/useModal";
+import BaseDeleteModal from "./BaseDeleteModal";
 
 const DeleteVitalModal: React.FC = () => {
   const { modalRef, activeModal, closeModal } = useModal();
@@ -26,29 +26,13 @@ const DeleteVitalModal: React.FC = () => {
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div ref={modalRef} className="bg-background rounded-md w-[500px]">
-        <div className="bg-destructive p-4">
-          <h3 className="text-lg font-medium text-destructive-foreground">
-            Are you sure?
-          </h3>
-        </div>
-
-        <form onSubmit={handleDeleteVital} className="p-4">
-          <p className="mb-6 text-gray-600 dark:text-gray-200 text-center">
-            Deleting this item is irreversible. Please confirm your action.
-          </p>
-          <div className="col-span-2 mt-6 flex justify-end space-x-2">
-            <Button variant="outline" onClick={closeModal}>
-              Cancel
-            </Button>
-            <Button type="submit" variant="destructive">
-              Delete
-            </Button>
-          </div>
-        </form>
-      </div>
-    </div>
+    <>
+      <BaseDeleteModal
+        modalRef={modalRef}
+        onSubmit={handleDeleteVital}
+        closeModal={closeModal}
+      />
+    </>
   );
 };
 
