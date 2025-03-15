69b75720d09cdb1a73b77844c9bb18ee00ff70f0
Lee Chun Yang <40287858+leechunyang98@users.noreply.github.com>
2025-03-16 02:20:08 +0800

    minor fix

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -12,7 +12,7 @@ const Searchbar: React.FC<SearchbarProps> = memo(
     return (
       <header className="sticky top-0 z-30 flex h-14 items-center gap-4 bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
         <div className="relative ml-auto flex-1 md:grow-0">
-          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
           <Input
             type="search"
             placeholder="Search..."
