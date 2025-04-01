90e052b662ea72605b96b584e685867662f473cb
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-04-01 15:52:52 +0800

    add personal info to account info tab

--- src/components/Tab/AccountInfoTab.tsx
+++ src/components/Tab/AccountInfoTab.tsx
@@ -1,22 +1,46 @@
 import { useViewAccount } from "@/hooks/admin/useViewAccount";
-import { TabsContent } from "../ui/tabs";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const AccountInfoTab: React.FC = () => {
   const { accountInfo } = useViewAccount();
-  
+
   return (
     <>
       <Card>
-            <CardHeader>
-              <CardTitle className="text-lg">Account Information</CardTitle>
-            </CardHeader>
-            <CardContent className="grid gap-2 md:grid-cols-2">
-              <div className="space-y-2">
-                {accountInfo?.email}
-              </div>
-            </CardContent>
-          </Card>
+        <CardHeader>
+          <CardTitle className="text-lg">Personal Information</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-2 md:grid-cols-2">
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Full Name</p>
+            {accountInfo?.nric_FullName || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Preferred Name</p>
+            {accountInfo?.preferredName || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">NRIC</p>
+            {accountInfo?.nric || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Gender</p>
+            {accountInfo?.nric_Gender || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Contact No</p>
+            {accountInfo?.contactNo || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Date of Birth</p>
+            {accountInfo?.nric_DateOfBirth || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Address</p>
+            {accountInfo?.nric_Address || "NA"}
+          </div>
+        </CardContent>
+      </Card>
     </>
   );
 };
