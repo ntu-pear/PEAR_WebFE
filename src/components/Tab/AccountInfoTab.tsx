9dc8478ef2e69188159f5990024e8a2b282a31d5
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-04-03 09:48:40 +0800

    add rest of the fields to account info tab

--- src/components/Tab/AccountInfoTab.tsx
+++ src/components/Tab/AccountInfoTab.tsx
@@ -36,8 +36,76 @@ const AccountInfoTab: React.FC = () => {
             {accountInfo?.nric_DateOfBirth || "NA"}
           </div>
           <div className="space-y-2">
+            <p className="text-sm font-medium">Email</p>
+            {accountInfo?.email || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Email Confirmed</p>
+            {accountInfo?.emailConfirmed ? "Yes" : "No"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Notifications Allowed</p>
+            {accountInfo?.allowNotification ? "Yes" : "No"}
+          </div>
+          <div className="space-y-2">
             <p className="text-sm font-medium">Address</p>
             {accountInfo?.nric_Address || "NA"}
+          </div>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-lg">Account Information</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-2 md:grid-cols-2">
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Account Status</p>
+            {accountInfo?.status || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Account Verified</p>
+            {accountInfo?.verified ? "Yes" : "No"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Account ID</p>
+            {accountInfo?.id || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Role</p>
+            {accountInfo?.roleName || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Two Factor Enabled</p>
+            {accountInfo?.twoFactorEnabled ? "Yes" : "No"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Lockout Enabled</p>
+            {accountInfo?.lockoutEnabled ? "Yes" : "No"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Lockout Reason</p>
+            {accountInfo?.lockoutReason || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Login Timestamp</p>
+            {accountInfo?.loginTimeStamp || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Created By</p>
+            {accountInfo?.createdById || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Created Date</p>
+            {accountInfo?.createdDate || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Modified By</p>
+            {accountInfo?.modifiedById || "NA"}
+          </div>
+          <div className="space-y-2">
+            <p className="text-sm font-medium">Modified Date</p>
+            {accountInfo?.modifiedDate || "NA"}
           </div>
         </CardContent>
       </Card>
