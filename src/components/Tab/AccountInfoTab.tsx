683fcaacbb2386ac5ae214f711a5402d46e6d0e5
Owen-Choh <94509487+Owen-Choh@users.noreply.github.com>
2025-05-20 15:30:10 +0800

    format the view account tab to be easier to test

--- src/components/Tab/AccountInfoTab.tsx
+++ src/components/Tab/AccountInfoTab.tsx
@@ -22,16 +22,16 @@ const AccountInfoTab: React.FC = () => {
         <CardContent className="grid gap-2 md:grid-cols-2">
           <div className="space-y-2">
             <p className="text-sm font-medium">NRIC Full Name</p>
-            {accountInfo?.nric_FullName || "NA"}
+            <p id="accountinfo-nric-fullname">{accountInfo?.nric_FullName || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Preferred Name</p>
-            {accountInfo?.preferredName || "NA"}
+            <p id="accountinfo-preferred-name">{accountInfo?.preferredName || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">NRIC</p>
             <div className="flex">
-              {getNRIC() || "NA"}
+              <p id="accountinfo-nric">{getNRIC() || "NA"}</p>
               <Button
                 size="icon"
                 variant="ghost"
@@ -48,31 +48,31 @@ const AccountInfoTab: React.FC = () => {
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Gender</p>
-            {accountInfo?.nric_Gender || "NA"}
+            <p id="accountinfo-gender">{accountInfo?.nric_Gender || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Contact No</p>
-            {accountInfo?.contactNo || "NA"}
+            <p id="accountinfo-contact-no">{accountInfo?.contactNo || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Date of Birth</p>
-            {accountInfo?.nric_DateOfBirth || "NA"}
+            <p id="accountinfo-dob">{accountInfo?.nric_DateOfBirth || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Email</p>
-            {accountInfo?.email || "NA"}
+            <p id="accountinfo-email">{accountInfo?.email || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Email Confirmed</p>
-            {accountInfo?.emailConfirmed ? "Yes" : "No"}
+            <p id="accountinfo-email-confirmed">{accountInfo?.emailConfirmed ? "Yes" : "No"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Notifications Allowed</p>
-            {accountInfo?.allowNotification ? "Yes" : "No"}
+            <p id="accountinfo-notifications-allowed">{accountInfo?.allowNotification ? "Yes" : "No"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">NRIC Address</p>
-            {accountInfo?.nric_Address || "NA"}
+            <p id="accountinfo-nric-address">{accountInfo?.nric_Address || "NA"}</p>
           </div>
         </CardContent>
       </Card>
@@ -84,55 +84,59 @@ const AccountInfoTab: React.FC = () => {
         <CardContent className="grid gap-2 md:grid-cols-2">
           <div className="space-y-2">
             <p className="text-sm font-medium">Account Status</p>
-            {accountInfo?.isDeleted ? "Inactive" : "Active"}
+            <p id="accountinfo-status">{accountInfo?.isDeleted ? "Inactive" : "Active"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Account Verified</p>
-            {accountInfo?.verified ? "Yes" : "No"}
+            <p id="accountinfo-verified">{accountInfo?.verified ? "Yes" : "No"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Account ID</p>
-            {accountInfo?.id || "NA"}
+            <p id="accountinfo-id">{accountInfo?.id || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Role</p>
-            {accountInfo?.roleName || "NA"}
+            <p id="accountinfo-role">{accountInfo?.roleName || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Two Factor Enabled</p>
-            {accountInfo?.twoFactorEnabled ? "Yes" : "No"}
+            <p id="accountinfo-2fa">{accountInfo?.twoFactorEnabled ? "Yes" : "No"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Lockout Enabled</p>
-            {accountInfo?.lockOutEnabled ? "Yes" : "No"}
+            <p id="accountinfo-lockout-enabled">{accountInfo?.lockOutEnabled ? "Yes" : "No"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Lockout Reason</p>
-            {accountInfo?.lockOutReason || "NA"}
+            <p id="accountinfo-lockout-reason">{accountInfo?.lockOutReason || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Login Timestamp</p>
-            {accountInfo?.loginTimeStamp || "NA"}
+            <p id="accountinfo-login-timestamp">{accountInfo?.loginTimeStamp || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Created By</p>
-            {createdByAccount
-              ? `${createdByAccount.nric_FullName} (Account ID: ${createdByAccount.id})`
-              : `${"Account ID: " + accountInfo?.createdById || "NA"}`}
+            <p id="accountinfo-created-by">
+              {createdByAccount
+                ? `${createdByAccount.nric_FullName} (Account ID: ${createdByAccount.id})`
+                : `${"Account ID: " + accountInfo?.createdById || "NA"}`}
+            </p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Created Date</p>
-            {accountInfo?.createdDate || "NA"}
+            <p id="accountinfo-created-date">{accountInfo?.createdDate || "NA"}</p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Modified By</p>
-            {modifiedByAccount
-              ? `${modifiedByAccount.nric_FullName} (Account ID: ${modifiedByAccount.id})`
-              : `${"Account ID: " + accountInfo?.modifiedById || "NA"}`}
+            <p id="accountinfo-modified-by">
+              {modifiedByAccount
+                ? `${modifiedByAccount.nric_FullName} (Account ID: ${modifiedByAccount.id})`
+                : `${"Account ID: " + accountInfo?.modifiedById || "NA"}`}
+            </p>
           </div>
           <div className="space-y-2">
             <p className="text-sm font-medium">Modified Date</p>
-            {accountInfo?.modifiedDate || "NA"}
+            <p id="accountinfo-modified-date">{accountInfo?.modifiedDate || "NA"}</p>
           </div>
         </CardContent>
       </Card>
