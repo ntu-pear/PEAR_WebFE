7b7197d6c0ad290ee63fd07ebb9a874e8e2ccac4
Cebelle1 <cebelleloo@hotmail.com>
2025-09-26 18:49:34 +0800

    - FIx merge conflicts

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -12,23 +12,21 @@ const VITE_GEOCODE_SERVICE_URL: string = import.meta.env
 // console.log('Patient Service URL:' + VITE_PATIENT_SERVICE_URL);
 const VITE_LOGGER_SERVICE_URL: string = import.meta.env.VITE_LOGGER_SERVICE_URL;
 
-// activity
+
+export const VITE_USER_SERVICE_URL: string = import.meta.env
+  .VITE_USER_SERVICE_URL;
+
+// activity service
+export const activityAPI = axios.create({
+  baseURL: `${VITE_ACTIVITY_SERVICE_URL}`,
+});
+
 export const activitiesAPI = axios.create({
   baseURL: `${VITE_ACTIVITY_SERVICE_URL}/activities`,
 });
 
 export const careCentreAPI = axios.create({
   baseURL: `${VITE_ACTIVITY_SERVICE_URL}/care_centres`,
-});
-
-export const VITE_USER_SERVICE_URL: string = import.meta.env
-  .VITE_USER_SERVICE_URL;
-
-const VITE_ACTIVITY_SERVICE_URL: string = import.meta.env.VITE_ACTIVITY_SERVICE_URL;
-
-// activity service
-export const activityAPI = axios.create({
-  baseURL: `${VITE_ACTIVITY_SERVICE_URL}`,
 });
 
 // patient
