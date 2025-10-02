e2b949f16e0b8c0073707624e517e8946cbb0108
Cebelle1 <cebelleloo@hotmail.com>
2025-10-02 20:53:01 +0800

    - Add bulk selection of Preference records - Change Edit Pref Modal to just buttons

--- src/components/Tab/ActivityPreferenceTab.tsx
+++ src/components/Tab/ActivityPreferenceTab.tsx
@@ -1,11 +1,8 @@
 import { TabsContent } from "../ui/tabs";
-import { useModal } from "@/hooks/useModal";
 import { useViewPatient } from "@/hooks/patient/useViewPatient";
 import PatientActivityPreferenceCard from "../Card/PatientActivityPreferenceCard";
-import EditActivityPreferenceModal from "../Modal/Edit/EditActivityPreferenceModal";
 
 const ActivityPreferenceTab: React.FC = () => {
-  const { activeModal } = useModal();
   const { id: patientId } = useViewPatient();
 
   if (!patientId) {
@@ -19,18 +16,13 @@ const ActivityPreferenceTab: React.FC = () => {
   }
 
   return (
-    <>
-      <TabsContent value="activity-preference">
-        <div className="my-2">
-          <PatientActivityPreferenceCard 
-            patientId={patientId}
-          />
-        </div>
-      </TabsContent>
-      {activeModal.name === "editActivityPreference" && (
-        <EditActivityPreferenceModal />
-      )}
-    </>
+    <TabsContent value="activity-preference">
+      <div className="my-2">
+        <PatientActivityPreferenceCard 
+          patientId={patientId}
+        />
+      </div>
+    </TabsContent>
   );
 };
 
