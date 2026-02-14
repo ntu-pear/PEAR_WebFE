3b7ef1848d4f07d08d11b1ad0912540c87535c27
Ng Shi Wen <122603053+potatopotati@users.noreply.github.com>
2026-02-14 23:40:16 +0800

    fix pagination in medical history, mobility aids and routine table. implement problem log table

--- src/components/Modal/Add/AddProblemModal.tsx
+++ src/components/Modal/Add/AddProblemModal.tsx
@@ -1,45 +1,113 @@
 import { useModal } from "@/hooks/useModal";
 import { Button } from "../../ui/button";
+import { useEffect, useState } from "react";
+import { addPatientProblemLog, AddProblemLog, getProblemList, ProblemList } from "@/api/patients/problemLog";
+import { useViewPatient } from "@/hooks/patient/useViewPatient";
+import { toast } from "sonner";
 
 const AddProblemModal: React.FC = () => {
-  const { modalRef, closeModal } = useModal();
-  const handleAddProblem = (event: React.FormEvent) => {
+  const { modalRef, closeModal, activeModal } = useModal();
+  const [problemList, setProblemList] = useState<ProblemList[]>()
+  const { id } = useViewPatient()
+  const { refreshData } = activeModal.props as {
+    refreshData: () => void
+  }
+  const handleAddProblem = async (event: React.FormEvent) => {
     event.preventDefault();
-    console.log("Patient Problem Added!");
-    closeModal();
+    const formData = new FormData(event.target as HTMLFormElement)
+    const formDataObj = Object.fromEntries(formData.entries())
+    const newProblemLog: AddProblemLog = {
+      PatientID: Number(id),
+      ProblemListID: Number(formDataObj.ProblemListID),
+      DateOfDiagnosis: formDataObj.DateOfDiagnosis as string,
+      ProblemRemarks: formDataObj.ProblemRemarks as string,
+      SourceOfInformation: formDataObj.SourceOfInformation as string
+    }
+    try {
+      await addPatientProblemLog(newProblemLog)
+      console.log("Patient Problem Added!");
+      toast.success("Sucessfully added Patient Problem Log")
+      await refreshData();
+      closeModal();
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(`Failed to add Problem Log. ${error}`)
+      } else {
+        toast.error('Failed to add Problem Log')
+      }
+      console.error("Failed to add medical history")
+      closeModal()
+    }
   };
+
+  useEffect(() => {
+    const fetchProblemList = async () => {
+      try {
+        const list = await getProblemList()
+        setProblemList(list)
+      } catch (error) {
+
+      }
+    }
+    fetchProblemList()
+  }, [])
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div ref={modalRef} className="bg-background p-8 rounded-md w-[400px]">
-        <h3 className="text-lg font-medium mb-5">Add Problem</h3>
+        <h3 className="text-lg font-medium mb-5">Add Problem Log</h3>
         <form onSubmit={handleAddProblem} className="grid grid-cols-2 gap-4">
           <div className="col-span-2">
             <label className="block text-sm font-medium">
-              Problem<span className="text-red-600">*</span>
+              Problem Name<span className="text-red-600">*</span>
             </label>
             <select
               className="mt-1 block w-full p-2 border rounded-md text-gray-900"
+              name="ProblemListID"
               required
             >
               <option value="">Please select an option</option>
-              <option value="Behavior">Behavior</option>
-              <option value="Communication">Communication</option>
-              <option value="Delusion">Delusion</option>
-              <option value="Emotion">Emotion</option>
-              <option value="Health">Health</option>
-              <option value="Memory">Memory</option>
-              <option value="Sleep">Sleep</option>
+              {
+                problemList?.map((list) => (
+                  <option key={list.Id} value={list.Id}>{list.ProblemName}</option>
+                ))
+              }
             </select>
           </div>
 
           <div className="col-span-2">
             <label className="block text-sm font-medium">
-              Notes<span className="text-red-600">*</span>
+              Source of Information <span className="text-red-600">*</span>
+            </label>
+            <input
+              type="text"
+              name="SourceOfInformation"
+              className="mt-1 block w-full p-2 border rounded-md text-gray-900"
+              required>
+            </input>
+          </div>
+
+          <div className="col-span-2">
+            <label className="block text-sm font-medium">
+              Remarks<span className="text-red-600">*</span>
             </label>
             <textarea
+              name="ProblemRemarks"
               className="mt-1 block w-full p-2 border rounded-md text-gray-900"
               required
+            />
+          </div>
+
+          <div className="col-span-2">
+            <label className="block text-sm font-medium">
+              Date of Diagnosis<span className="text-red-600">*</span>
+            </label>
+            <input
+              type="date"
+              name="DateOfDiagnosis"
+              className="mt-1 block w-full p-2 border rounded-md text-gray-900"
+              required
+              max={new Date().toISOString().split("T")[0]}
             />
           </div>
 
