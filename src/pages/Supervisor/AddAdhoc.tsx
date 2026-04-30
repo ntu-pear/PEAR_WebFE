8d502e0d68385ca9719af2370b3aeeebdb5988d1
LightningJason <jasonlimjiasheng@gmail.com>
2026-04-30 17:50:05 +0800

    changed add adhoc  button to black

--- src/pages/Supervisor/AddAdhoc.tsx
+++ src/pages/Supervisor/AddAdhoc.tsx
@@ -426,7 +426,16 @@ const AddAdhoc: React.FC = () => {
               </button>
               <button
                 type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                
+                className="
+                    rounded-md bg-black px-3 py-2
+                    text-sm font-semibold text-white
+                    shadow-sm
+                    hover:bg-gray-800
+                    focus-visible:outline focus-visible:outline-2
+                    focus-visible:outline-offset-2 focus-visible:outline-black
+                  "
+
               >
                 Add Adhoc
               </button>
